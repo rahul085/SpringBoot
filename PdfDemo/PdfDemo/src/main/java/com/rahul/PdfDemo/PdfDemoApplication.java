a32f26021eb4e707ceb5ead30a43eacc701d4f49
Rahul <rahulneeli2003@gmail.com>
2025-10-09 14:37:12 +0530

    Java pdf and excel handling

---
+++ PdfDemo/PdfDemo/src/main/java/com/rahul/PdfDemo/PdfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.PdfDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfDemoApplication.class, args);
+	}
+
+}
